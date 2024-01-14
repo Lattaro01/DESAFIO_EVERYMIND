698688b57818501663acc92902f7b3d010d680b4
Lattaro01 <lattaro01@hotmail.com>
2024-01-14 01:34:58 -0300

    refactor

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ const db = mysql.createConnection({
     database: DESAFIO_EVERYMIND
 });
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/produtos', (req, res));
+app.post('/produtos', (req, res));
+app.put('/produtos/:id' (req, res));
+app.delete('/produto/:id' (req, res));
+app.listen(port, (){
+  console.log('Server listening at jdbc:mysql://127.0.0.1:3306/?user=root')
+});
